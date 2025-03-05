cf47cc47fda656fb8f606fa5b9c115e61983c984
Mark-Lua <s62711@parkhill.k12.mo.us>
2025-03-04 19:27:03 -0600

    Fixed Algae Pivot

--- src/main/java/frc/robot/subsystems/AlgaePivot.java
+++ src/main/java/frc/robot/subsystems/AlgaePivot.java
@@ -22,14 +22,18 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.constants.AlgaePivotConstants;
 import frc.robot.constants.CANConstants;
 import frc.robot.constants.CoralLevels;
+import java.util.function.Supplier;
 
 public class AlgaePivot extends SubsystemBase {
   private MotionMagicVoltage m_motionMatiVoltage = new MotionMagicVoltage(0).withSlot(0);
   private TalonFXS m_AlgaePivotTalon;
   private Trigger hasAlgae;
+  private Supplier<CoralLevels> coralLevelSupplier;
 
-  public AlgaePivot(Trigger hasAlgae) {
+  public AlgaePivot(Trigger hasAlgae, Supplier<CoralLevels> coralLevelSupplier) {
     this.hasAlgae = hasAlgae;
+    this.coralLevelSupplier = coralLevelSupplier;
+
     TalonFXSConfiguration talonFXSConfiguration = new TalonFXSConfiguration();
 
     talonFXSConfiguration.Slot0.kP = AlgaePivotConstants.kP;
@@ -86,9 +90,16 @@ public class AlgaePivot extends SubsystemBase {
         });
   }
 
+  public boolean shouldGoToMidPoint() {
+    if (hasAlgae.getAsBoolean() && !coralLevelSupplier.get().name().equals("PROCESSER")) {
+      return true;
+    }
+    return false;
+  }
+
   public Command goToLevel(CoralLevels level) {
     ConditionalCommand command =
-        new ConditionalCommand(goToMidPoint(), rotateToLevel(level), hasAlgae);
+        new ConditionalCommand(goToMidPoint(), rotateToLevel(level), this::shouldGoToMidPoint);
 
     return command;
   }
