c6abca28e12a38d311f83c8cd4bbd74902f28a0c
RoWiz09 <robotwizard09@gmail.com>
2025-03-06 13:52:55 -0600

    Renamed autos, changed coral level values, added algae trim

--- src/main/java/frc/robot/subsystems/AlgaePivot.java
+++ src/main/java/frc/robot/subsystems/AlgaePivot.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.configs.TalonFXSConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFXS;
 import com.ctre.phoenix6.signals.ExternalFeedbackSensorSourceValue;
 import com.ctre.phoenix6.signals.InvertedValue;
@@ -16,6 +17,7 @@ import com.ctre.phoenix6.signals.SensorPhaseValue;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -118,6 +120,22 @@ public class AlgaePivot extends SubsystemBase {
 
   private void rotate(Angle setpoint) {
     m_AlgaePivotTalon.setControl(m_motionMatiVoltage.withPosition(setpoint));
+  }
+
+  public Command openLoopCommandUp(){
+    return runOnce(
+      ()->{openLoop(0.1);}
+    );
+  }
+
+  public Command openLoopCommandDown(){
+    return runOnce(
+      ()->{openLoop(-0.1);}
+    );
+  }
+
+  private void openLoop(double power){
+    m_AlgaePivotTalon.setControl(new DutyCycleOut(power));
   }
 
   private Command rotateToLevel(CoralLevels level) {
