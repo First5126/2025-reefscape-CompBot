d4c0384bc0866c172949fc36287cea24cb25da65
RoWiz09 <robotwizard09@gmail.com>
2025-03-07 20:42:36 -0600

    fixed merge conflicts

--- src/main/java/frc/robot/subsystems/CommandFactory.java
+++ src/main/java/frc/robot/subsystems/CommandFactory.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.Seconds;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -244,7 +245,7 @@ public class CommandFactory {
     Command ReleaseCoral = m_coralRollers.rollOutCommand(level);
     Command raiseElevator = m_elevator.setCoralPosition(level);
 
-    return raiseElevator.alongWith(pivotCoralRollersCommand).andThen(ReleaseCoral);
+    return raiseElevator.alongWith(pivotCoralRollersCommand).withTimeout(0.25).andThen(ReleaseCoral);
   }
 
   public Command moveElevatorUpToL2() {
