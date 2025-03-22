2435d4536c2ffd1f0c5a2c45e6ac77fa6e2c3809
Mark-Lua <s62711@parkhill.k12.mo.us>
2025-03-22 14:40:48 -0500

    Removed LED from driver

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,7 +19,6 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -226,7 +225,6 @@ public class RobotContainer {
                 VisonAdjustment::getInversion));
 
     m_driverController.start().onTrue(m_commandFactory.zeroRobot());
-    m_driverController.back().onTrue(m_ledLights.applyColor(new Color8Bit(10, 100, 50)));
 
     m_driverController.b().whileTrue(m_commandFactory.moveBack());
     // Bumpers to coral station
