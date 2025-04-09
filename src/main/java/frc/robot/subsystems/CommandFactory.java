855cc6b7b75a36d3dc244c0c64128ec9ca7ba1fb
RoWiz09 <robotwizard09@gmail.com>
2025-04-09 16:26:23 -0500

    Adding a Flip To work in the Autos.

--- src/main/java/frc/robot/subsystems/CommandFactory.java
+++ src/main/java/frc/robot/subsystems/CommandFactory.java
@@ -249,9 +249,10 @@ public class CommandFactory {
   private Command placeCoralonL4() {
     System.out.println("Placing Coral L4");
     Command ReleaseCoral = m_coralRollers.rollOutCommand(CoralLevels.L4);
+    Command flip = m_coralPivot.goToUpperSetpoint();
 
     SmartDashboard.putData("Place Coral L4", ReleaseCoral);
-    return ReleaseCoral;
+    return ReleaseCoral.andThen(flip).withTimeout(0.1);
   }
 
   private Command placeCoralonL3() {
